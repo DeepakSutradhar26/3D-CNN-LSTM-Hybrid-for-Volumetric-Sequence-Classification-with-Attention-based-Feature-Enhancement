de616350bc61851cf7bba5d843880f47422d4c0c
Deepa <deepaksutradhar415@gmail.com>
2026-01-18 18:36:38 +0530

    train-first-commit

--- src/architecture/LSTM.py
+++ src/architecture/LSTM.py
@@ -10,10 +10,17 @@ class CNN_LSTM(nn.Module):
         self.lstm = nn.LSTM(
             input_size=128,
             hidden_size=128,
+            dropout=0.5,
             batch_first=True,
+        )
+
+        self.final_layer = nn.Sequential(
+            nn.Linear(128, 1),
+            nn.Sigmoid(),
         )
 
     def forward(self, x):
         x = self.cnn(x)
         x, _ = self.lstm(x)
+        x = self.final_layer(x)
         return x
