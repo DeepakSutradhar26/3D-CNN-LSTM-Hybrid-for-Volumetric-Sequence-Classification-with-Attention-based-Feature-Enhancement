b5840cd9ae14e1169834ef3fd711003437b9c60b
Deepa <deepaksutradhar415@gmail.com>
2026-01-18 21:54:21 +0530

    data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,20 @@
 from torch.utils.data import DataLoader
+from dataset import MRIDataset
+import metrics
+import os
+
+train_loader = DataLoader(
+    MRIDataset(os.path.join(metrics.NDATA_DIR, "train")),
+    batch_size = metrics.BATCH_SIZE,
+    num_workers = metrics.NUM_WORKERS,
+    shuffle = True,
+    pin_memory = True,
+)
+
+test_loader = DataLoader(
+    MRIDataset(os.path.join(metrics.NDATA_DIR, "test")),
+    batch_size = metrics.BATCH_SIZE,
+    num_workers = metrics.NUM_WORKERS,
+    shuffle = True,
+    pin_memory = True,
+)
