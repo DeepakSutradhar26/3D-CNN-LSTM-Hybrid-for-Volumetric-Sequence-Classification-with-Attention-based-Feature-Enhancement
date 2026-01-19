76e7f9f43d3374482b9cd86d48e43d05e8e179b9
Deepa <deepaksutradhar415@gmail.com>
2026-01-19 11:59:30 +0530

    bugs

--- src/architecture/cnn3.py
+++ src/architecture/cnn3.py
@@ -1,7 +1,19 @@
 import torch
 import torch.nn as nn
 
-import SE
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super().__init__()
+        self.se_layer = nn.Sequential(
+            nn.AdaptiveAvgPool3d(1),
+            nn.Conv3d(in_channels, in_channels // reduction, 1),
+            nn.ReLU(),
+            nn.Conv3d(in_channels // reduction, in_channels, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.se_layer(x)
 
 class CNNArchitecture(nn.Module):
     def __init__(self, input_shape=(128, 128, 32)):
@@ -39,7 +51,7 @@ class CNNArchitecture(nn.Module):
             nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True),
 
-            SE.SEBlock(out_channels),
+            SEBlock(out_channels),
             nn.MaxPool3d(kernel_size=2, stride=2),
         )
     
